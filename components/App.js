fc78bed56d1bc008de858669034ef632076922e8
ani <anna.unton@yahoo.com>
2018-02-18 17:52:23 +0700

    initial commit

--- components/App.js
+++ components/App.js
@@ -1,40 +1,67 @@
-var contacts1 = [
-  {
-    id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
-  },
-  {
-    id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
-  },
-  {
-    id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
-  }
-];
+App = React.createClass({
 
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: ''
-};
+	getInitialState() {
+    return {
+        loading: false,
+        searchingText: '',
+        gif: {}
+    	};
+	},
 
-var App = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts1}, {})
-      )
-    );
-  }
+	handleSearch: function(searchingText) {  // 1.
+    	this.setState({
+      		loading: true  // 2.
+    	});
+    	this.getGif(searchingText, function(gif) {  // 3.
+      		this.setState({  // 4
+        	loading: false,  // a
+        	gif: gif,  // b
+        	searchingText: searchingText  // c
+      		});
+    	}.bind(this));
+  	},
+
+  	getGif: function(searchingText, callback) {  // 1.
+    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+    var xhr = new XMLHttpRequest();  // 3.
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+           var data = JSON.parse(xhr.responseText).data; // 4.
+            var gif = {  // 5.
+                url: data.fixed_width_downsampled_url,
+                sourceUrl: data.url
+            };
+            callback(gif);  // 6.
+        }
+    };
+    xhr.send();
+},
+
+    render: function() {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (
+          <div style={styles}>
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+      
+                <Search onSearch={this.handleSearch}/>
+            
+            <Gif
+    		loading={this.state.loading}
+    		url={this.state.gif.url}
+    		sourceUrl={this.state.gif.sourceUrl}
+			/>
+
+          </div>
+        );
+    }
 });
-
 
 
